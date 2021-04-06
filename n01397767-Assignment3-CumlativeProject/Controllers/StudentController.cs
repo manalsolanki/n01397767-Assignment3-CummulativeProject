de1c9d0135fd41eae8b37415abd2b875501e7788
Manal Solanki <97manal@gmail.com>
2021-04-06 04:29:18 -0400

    Added the student Module.

---
+++ n01397767-Assignment3-CumlativeProject/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using n01397767_Assignment3_CumlativeProject.Models;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace n01397767_Assignment3_CumlativeProject.Controllers
+{
+    //An intermediate between view and Web api controller.
+
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Helps to display the List view.That is list of all Student.
+        /// </summary>
+        /// <param name="SearchKey">To search by Name.</param>
+        /// <returns>A view with list of Student objects</returns>
+        public ActionResult List( string searchKey=null)
+        {
+            //Connects through the webapi controller.
+            StudentDataController controller = new StudentDataController();
+            //Calls a method of web Api controller and stores in a list of Student object.
+            IEnumerable<Student> Students = controller.ListStudents(searchKey);
+
+            //Returns a list of students.
+            return View(Students);
+        }
+
+
+        // GET : Teacher/Show/{id}
+        /// <summary>
+        /// This methods help to display a teacher property or details of a teacher.
+        /// </summary>
+        /// <param name="id">Helps to find by id</param>
+        /// <returns>A view with information of a particular teacher(id)</returns>
+        public ActionResult Show(int id)
+        {
+            //Connects through the webapi controller.
+            StudentDataController controller = new StudentDataController();
+            //Calls a method of web Api controller and stores in a particular information of Student object.
+            Student NewStudent = controller.FindStudent(id);
+            Debug.WriteLine("The student detail is");
+            Debug.WriteLine(NewStudent);
+
+            //Returns the evalute result.
+            return View(NewStudent);
+        }
+    }
+}
--- n01397767-Assignment3-CumlativeProject/Controllers/StudentDataController.cs
+++ n01397767-Assignment3-CumlativeProject/Controllers/StudentDataController.cs
@@ -23,7 +23,7 @@ namespace n01397767_Assignment3_CumlativeProject.Controllers
         /// <returns>A list of Student Object.</returns>
         [HttpGet]
         [Route("api/StudentData/ListStudents/{searchKey?}")]
-        public IEnumerable<Student> ListStudents(string searchKey = null)
+        public IEnumerable<Student> ListStudents(string searchKey)
         {
             //Instance of a connection using MySQL object.
             MySqlConnection Connection = School.AccessDatabase();
@@ -35,16 +35,16 @@ namespace n01397767_Assignment3_CumlativeProject.Controllers
             MySqlCommand cmd = Connection.CreateCommand();
 
 
-            //SQL QUERY which finds list of teacher using where clause.(using searchkey)
-            cmd.CommandText = "Select * from students ";
-
+            //SQL QUERY which finds list of student using where clause.(using searchkey)
+            cmd.CommandText = "Select * from students where lower(studentfname) like lower(@key) or lower(studentlname) like lower(@key) or lower(concat(studentfname, ' ', studentlname)) like lower(@key)";
+           
             // Replacing the @key value in the above query with actually value.
-           // cmd.Parameters.AddWithValue("@key", "%" + searchKey + "%");
+            cmd.Parameters.AddWithValue("@key", "%" + searchKey + "%");
 
             //When Query runs using executeReader the result is stored in Resultset
             MySqlDataReader ResultSet = cmd.ExecuteReader();
 
-            //Empty list of Teacher Names to store all the data from the DB.
+            //Empty list of Student Names to store all the data from the DB.
             List<Student> StudentNames = new List<Student> { };
 
             //Iterating through each row of the result computed in result set.
@@ -59,10 +59,10 @@ namespace n01397767_Assignment3_CumlativeProject.Controllers
                 DateTime studentEnrolDate = (DateTime)ResultSet["enroldate"];
                 string date = studentEnrolDate.ToLongDateString();
 
-                // Creating a NewTeacher object of type Teacher to store the information of one teacher at a time.
+                // Creating a NewStudent object of type Student to store the information of one Student at a time.
                 Student NewStudent = new Student();
 
-                //Add the inoformation to the NewTeacher
+                //Add the inoformation to the NewStudent
 
                 NewStudent.StudentId = StudentId;
                 NewStudent.StudentFname = StudentfName;
@@ -70,17 +70,73 @@ namespace n01397767_Assignment3_CumlativeProject.Controllers
                 NewStudent.StudentNumber = studentNumber;
                 NewStudent.StudentEnrolDate = date;
 
-                // Then add this to the TeachersNames final List. 
+                // Then add this to the StudentsNames final List. 
                 StudentNames.Add(NewStudent);
             }
             //Important to close the connection between webserver and Database.
             Connection.Close();
 
-            //Return the final list of Teacher names
+            //Return the final list of Student names
             return StudentNames;
         }
 
+        ///<summary>
+        /// This methods helps to find the student detail with the help of primary key that is id.
+        /// </summary>
+        /// <param name="id">Student's Id the student Table of School Database.</param>
+        /// <returns>A student Object with all necessary information</returns>
+        [HttpGet]
+        [Route("api/StudentData/FindStudent/{id}")]
+        public Student FindStudent(int id)
+        {
+            //Instance of a connection using MySQL object.
+            MySqlConnection Connection = School.AccessDatabase();
 
-         
+            //Establishes connection between web server and the database
+            Connection.Open();
+
+            //This helps to create a new command of SQL.
+            MySqlCommand cmd = Connection.CreateCommand();
+
+
+            //SQL QUERY which finds list of student using where clause.(using searchkey)
+            cmd.CommandText = "Select * from students where studentid ="+id;
+
+            //When Query runs using executeReader the result is stored in Resultset
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            // Creating a NewStudent object of type Student to store the information 
+            Student NewStudent = new Student();
+
+            //Iterating through each row of the result computed in result set here it will one row.
+            while (ResultSet.Read())
+            {
+                //We can access each colunmname giving it as a index of the resut set.
+                // We need to typeCast each value into suitable Data type.
+                int StudentId = Convert.ToInt32(ResultSet["studentid"]);
+                string StudentfName = (string)ResultSet["studentfname"];
+                string studentlName = (string)ResultSet["studentlname"];
+                string studentNumber = (string)ResultSet["studentnumber"];
+                DateTime studentEnrolDate = (DateTime)ResultSet["enroldate"];
+                string date = studentEnrolDate.ToLongDateString();
+
+                
+
+                //Add the inoformation to the NewStudent
+
+                NewStudent.StudentId = StudentId;
+                NewStudent.StudentFname = StudentfName;
+                NewStudent.StudentLname = studentlName;
+                NewStudent.StudentNumber = studentNumber;
+                NewStudent.StudentEnrolDate = date;
+            }
+            //Important to close the connection between webserver and Database.
+            Connection.Close();
+
+            //Return the final list of Student names
+            return NewStudent;
+        }
+
+
     }
 }
